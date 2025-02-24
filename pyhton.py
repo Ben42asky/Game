196a659f1b637e99c6878856840abea205ab14b5
Ben42asky <odedebenjamin7@gmail.com>
2025-02-24 12:28:36 +0300

    intial commit

---
+++ pyhton.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Hello from Python on Vercel!')
+
+def handler(request):
+    return Handler()
